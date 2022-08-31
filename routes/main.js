3cb756e32fb68e365c8c3582f3d56e4298b01a8e
trstngttschlk <trstn@gttschlk.com>
2022-08-30 21:30:39 -0400

    add comments to code

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,12 @@
+// required packages
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
 const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+// pulling specific functions "ensureAuth" out of this file "../middleware/auth"
+const { ensureAuth } = require('../middleware/auth')
 
+// Routes
 router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Required Packages
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -6,7 +7,9 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
+// Middleware
 const connectDB = require('./config/database')
+// declare routes
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
@@ -15,13 +18,20 @@ require('dotenv').config({path: './config/.env'})
 // Passport config
 require('./config/passport')(passport)
 
+// Connect to database
 connectDB()
 
+// sets app to use ejs
 app.set('view engine', 'ejs')
+// set routes to our static files
 app.use(express.static('public'))
+// url encoding
 app.use(express.urlencoded({ extended: true }))
+// parse body in request from json
 app.use(express.json())
+// set logging in dev envirvonment
 app.use(logger('dev'))
+
 // Sessions
 app.use(
     session({
@@ -37,7 +47,8 @@ app.use(passport.initialize())
 app.use(passport.session()) // saves sessions
 
 app.use(flash())
-  
+
+// Routes
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
  
